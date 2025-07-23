b222ffa38daf680c9c4469236fb1bd38637128ea
marcelooliveira <mclricardo@gmail.com>
2025-07-22 23:46:38 -0300

    Components

---
+++ FlashCards/Web/Model/Question.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Model
+{
+    public class Question
+    {
+        public string Text { get; set; } = string.Empty;
+        public List<string> Options { get; set; } = new();
+        public List<int> CorrectOptions { get; set; } = new();
+        public int? SelectedOption { get; set; } = null;
+        public bool? WasCorrect { get; set; } = null;
+    }
+}
