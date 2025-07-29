1505f65788f752f9aaa801ffb470ac4b3575653c
marcelooliveira <mclricardo@gmail.com>
2025-07-28 21:03:33 -0300

    QuestionCard & QuestionOptionCard

--- FlashCards/Web/Model/Question.cs
+++ FlashCards/Web/Model/Question.cs
@@ -3,7 +3,7 @@
     public class Question
     {
         public string Text { get; set; } = string.Empty;
-        public List<string> Options { get; set; } = new();
+        public List<QuestionOption> QuestionOptions { get; set; } = new();
         public List<int> CorrectOptions { get; set; } = new();
         public int? SelectedOption { get; set; } = null;
         public bool? WasCorrect { get; set; } = null;
---
+++ FlashCards/Web/Model/QuestionOption.cs
@@ -0,0 +1,17 @@
+﻿namespace Web.Model
+{
+    public class QuestionOption
+    {
+        public QuestionOption(string text)
+        {
+            Text = text;
+        }
+
+        public int Index { get; set; }
+        public string Text { get; set; }
+        public bool Checked { get; set; }
+        public bool IsSelected { get; set; }
+        public bool IsCorrect { get; set; }
+        public string Explanation { get; set; }
+    }
+}
