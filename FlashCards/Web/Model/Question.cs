7e12830cb637b7e138c0bac62046457d8b78ff7f
marcelooliveira <mclricardo@gmail.com>
2025-07-28 23:19:15 -0300

    yaml questions

--- FlashCards/Web/Model/Question.cs
+++ FlashCards/Web/Model/Question.cs
@@ -2,7 +2,13 @@
 {
     public class Question
     {
+        public string Id { get; set; }
         public string Text { get; set; } = string.Empty;
+        public string Type { get; set; }
+        public string Explanation { get; set; }
+        public string Difficulty { get; set; }
+        public int Points { get; set; }
+        public List<string> Tags { get; set; }
         public List<QuestionOption> QuestionOptions { get; set; } = new();
         public List<int> CorrectOptions { get; set; } = new();
         public List<int> SelectedOptions { get; set; } = new();
