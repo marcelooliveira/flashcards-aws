53e98a3b5ebb69ab83d36a956066422a71562473
marcelooliveira <mclricardo@gmail.com>
2025-07-28 22:29:27 -0300

    Lock question

--- FlashCards/Web/Model/Question.cs
+++ FlashCards/Web/Model/Question.cs
@@ -5,7 +5,8 @@
         public string Text { get; set; } = string.Empty;
         public List<QuestionOption> QuestionOptions { get; set; } = new();
         public List<int> CorrectOptions { get; set; } = new();
-        public int? SelectedOption { get; set; } = null;
+        public List<int> SelectedOptions { get; set; } = new();
         public bool? WasCorrect { get; set; } = null;
+        public bool IsLocked { get; set; }
     }
 }
--- FlashCards/Web/Model/QuestionOption.cs
+++ FlashCards/Web/Model/QuestionOption.cs
@@ -9,7 +9,6 @@
 
         public int Index { get; set; }
         public string Text { get; set; }
-        public bool Checked { get; set; }
         public bool IsSelected { get; set; }
         public bool IsCorrect { get; set; }
         public string Explanation { get; set; }
