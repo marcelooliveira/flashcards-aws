20094f38f7ee0c52ca70d20675c894b6f51b9737
marcelooliveira <mclricardo@gmail.com>
2025-11-28 13:48:17 -0300

    Question Option Images

--- FlashCards/Web/Model/QuestionOption.cs
+++ FlashCards/Web/Model/QuestionOption.cs
@@ -2,14 +2,20 @@
 {
     public class QuestionOption
     {
-        public QuestionOption(int index, string text)
+        public QuestionOption(int index, string text, string img)
         {
             Index = index;
             Text = text;
+
+            if (!string.IsNullOrWhiteSpace(img))
+            {
+                Img = $"Exams/{img}";
+            }
         }
 
         public int Index { get; set; }
         public string Text { get; set; }
+        public string Img { get; set; }
         public bool IsSelected { get; set; }
         public bool IsCorrect { get; set; }
         public string Explanation { get; set; }
